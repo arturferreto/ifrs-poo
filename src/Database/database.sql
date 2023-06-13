704af7d5e5c9403b95edd2e9efba85d26c681a20
Artur Ferreto <arturpessuto@gmail.com>
2023-06-13 19:23:20 -0300

    refact: ajustes para funcionar corretamente o sql

--- src/Database/database.sql
+++ src/Database/database.sql
@@ -1,13 +1,13 @@
 CREATE TABLE cu_addresses(
-     id           INT AUTO_INCREMENT PRIMARY KEY,
-     number       VARCHAR(10)  NOT NULL,
-     street       VARCHAR(255) NOT NULL,
-     neighborhood VARCHAR(255) NOT NULL,
-     complement   VARCHAR(255) NOT NULL,
-     city         VARCHAR(255) NOT NULL,
-     state        VARCHAR(255) NOT NULL,
-     country      VARCHAR(255) NOT NULL,
-     postal_code  VARCHAR(255) NOT NULL
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    number       VARCHAR(10)  NOT NULL,
+    street       VARCHAR(255) NOT NULL,
+    neighborhood VARCHAR(255) NOT NULL,
+    complement   VARCHAR(255) NOT NULL,
+    city         VARCHAR(255) NOT NULL,
+    state        VARCHAR(255) NOT NULL,
+    country      VARCHAR(255) NOT NULL,
+    postal_code  VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cu_people(
@@ -17,19 +17,8 @@ CREATE TABLE cu_people(
     phone          VARCHAR(20)  NOT NULL,
     is_driver      BOOLEAN      NOT NULL,
     fav_address_id INT,
-    fav_vehicle_id INT,
 
-    FOREIGN KEY(fav_address_id) REFERENCES cu_addresses(id),
-    FOREIGN KEY(fav_vehicle_id) REFERENCES cu_vehicles(id)
-);
-
-CREATE TABLE cu_person_address(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    person_id  INT NOT NULL,
-    address_id INT NOT NULL,
-
-    FOREIGN KEY(person_id)  REFERENCES cu_people(id),
-    FOREIGN KEY(address_id) REFERENCES cu_addresses(id)
+    FOREIGN KEY(fav_address_id) REFERENCES cu_addresses(id)
 );
 
 CREATE TABLE cu_vehicles(
@@ -42,6 +31,15 @@ CREATE TABLE cu_vehicles(
     seats     INTEGER      NOT NULL,
 
     FOREIGN KEY(person_id) REFERENCES cu_people(id)
+);
+
+CREATE TABLE cu_person_address(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    person_id  INT NOT NULL,
+    address_id INT NOT NULL,
+
+    FOREIGN KEY(person_id)  REFERENCES cu_people(id),
+    FOREIGN KEY(address_id) REFERENCES cu_addresses(id)
 );
 
 CREATE TABLE cu_rides(
@@ -70,3 +68,6 @@ CREATE TABLE cu_passengers(
     FOREIGN KEY(address_id) REFERENCES cu_addresses(id)
 );
 
+ALTER TABLE cu_people
+    ADD COLUMN fav_vehicle_id INT,
+    ADD FOREIGN KEY (fav_vehicle_id) REFERENCES cu_vehicles(id);
