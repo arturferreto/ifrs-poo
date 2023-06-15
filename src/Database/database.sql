24227d90009453cd3acd6a1c800c5bbfdc898f62
Artur Ferreto <91576108+arturferreto@users.noreply.github.com>
2023-06-15 23:10:05 +0000

    refact: refazendo configuração do banco de dados

--- src/Database/database.sql
+++ src/Database/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cu_addresses(
+CREATE TABLE u142008557_cu.addresses(
     id           INT AUTO_INCREMENT PRIMARY KEY,
     number       VARCHAR(10)  NOT NULL,
     street       VARCHAR(255) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE cu_addresses(
     postal_code  VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cu_people(
+CREATE TABLE u142008557_cu.people(
     id             INT AUTO_INCREMENT PRIMARY KEY,
     name           VARCHAR(255) NOT NULL,
     email          VARCHAR(255) NOT NULL,
@@ -18,10 +18,10 @@ CREATE TABLE cu_people(
     is_driver      BOOLEAN      NOT NULL,
     fav_address_id INT,
 
-    FOREIGN KEY(fav_address_id) REFERENCES cu_addresses(id)
+    FOREIGN KEY(fav_address_id) REFERENCES addresses(id)
 );
 
-CREATE TABLE cu_vehicles(
+CREATE TABLE u142008557_cu.vehicles(
     id        INT AUTO_INCREMENT PRIMARY KEY,
     person_id INT          NOT NULL,
     plate     VARCHAR(10)  NOT NULL,
@@ -30,19 +30,19 @@ CREATE TABLE cu_vehicles(
     model     VARCHAR(255) NOT NULL,
     seats     INTEGER      NOT NULL,
 
-    FOREIGN KEY(person_id) REFERENCES cu_people(id)
+    FOREIGN KEY(person_id) REFERENCES people(id)
 );
 
-CREATE TABLE cu_person_address(
+CREATE TABLE u142008557_cu.person_address(
     id         INT AUTO_INCREMENT PRIMARY KEY,
     person_id  INT NOT NULL,
     address_id INT NOT NULL,
 
-    FOREIGN KEY(person_id)  REFERENCES cu_people(id),
-    FOREIGN KEY(address_id) REFERENCES cu_addresses(id)
+    FOREIGN KEY(person_id)  REFERENCES people(id),
+    FOREIGN KEY(address_id) REFERENCES addresses(id)
 );
 
-CREATE TABLE cu_rides(
+CREATE TABLE u142008557_cu.rides(
     id          INT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -51,23 +51,23 @@ CREATE TABLE cu_rides(
     address_id  INT          NOT NULL,
     ride_type   SMALLINT     NOT NULL,
 
-    FOREIGN KEY(person_id)  REFERENCES cu_people(id),
-    FOREIGN KEY(address_id) REFERENCES cu_addresses(id),
-    FOREIGN KEY(vehicle_id) REFERENCES cu_vehicles(id)
+    FOREIGN KEY(person_id)  REFERENCES people(id),
+    FOREIGN KEY(address_id) REFERENCES addresses(id),
+    FOREIGN KEY(vehicle_id) REFERENCES vehicles(id)
 );
 
-CREATE TABLE cu_passengers(
+CREATE TABLE u142008557_cu.passengers(
     id         INT AUTO_INCREMENT PRIMARY KEY,
     ride_id    INT  NOT NULL,
     person_id  INT  NOT NULL,
     address_id INT  NOT NULL,
     date       DATE NOT NULL,
 
-    FOREIGN KEY(ride_id)    REFERENCES cu_rides(id),
-    FOREIGN KEY(person_id)  REFERENCES cu_people(id),
-    FOREIGN KEY(address_id) REFERENCES cu_addresses(id)
+    FOREIGN KEY(ride_id)    REFERENCES rides(id),
+    FOREIGN KEY(person_id)  REFERENCES people(id),
+    FOREIGN KEY(address_id) REFERENCES addresses(id)
 );
 
-ALTER TABLE cu_people
+ALTER TABLE u142008557_cu.people
     ADD COLUMN fav_vehicle_id INT,
-    ADD FOREIGN KEY (fav_vehicle_id) REFERENCES cu_vehicles(id);
+    ADD FOREIGN KEY (fav_vehicle_id) REFERENCES vehicles(id);
